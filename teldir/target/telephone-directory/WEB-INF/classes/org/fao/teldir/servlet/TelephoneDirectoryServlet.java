39ebe725ed7989c767106ee3b4b007c65c773221
Giorgio Vespucci <giorgio.vespucci@fao.org>
2011-02-22 16:58:00 +0100

    Preparing first commit in FAO SVN repository: project renamed 'teldir'

---
+++ teldir/target/telephone-directory/WEB-INF/classes/org/fao/teldir/servlet/TelephoneDirectoryServlet.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package org.fao.teldir.servlet;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * @author Vespucci
+ *
+ */
+public class TelephoneDirectoryServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	public TelephoneDirectoryServlet() {
+		// TODO Auto-generated constructor stub
+	}
+
+}

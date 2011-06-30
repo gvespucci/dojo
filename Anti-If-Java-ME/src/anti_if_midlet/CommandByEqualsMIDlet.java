dbf8daed8347a1fb32e289b31826d6d7067f420b
Giorgio Vespucci <giorgio.vespucci@fao.org>
2011-06-30 11:46:48 +0200

    MIDlet with command resolution by if and case

---
+++ Anti-If-Java-ME/src/anti_if_midlet/CommandByEqualsMIDlet.java
@@ -0,0 +1,79 @@
+package anti_if_midlet;
+
+import javax.microedition.midlet.MIDlet;
+import javax.microedition.midlet.MIDletStateChangeException;
+
+import com.sun.lwuit.Command;
+import com.sun.lwuit.Dialog;
+import com.sun.lwuit.Display;
+import com.sun.lwuit.Form;
+import com.sun.lwuit.events.ActionEvent;
+import com.sun.lwuit.events.ActionListener;
+
+public class CommandByEqualsMIDlet extends MIDlet implements ActionListener {
+
+	private static final String CANCEL = "Cancel";
+	private static final String OK = "OK";
+	private static final int EXIT_COMMAND_ID = 1;
+	private static final int SAY_YES_COMMAND_ID = 2;
+	private static final int SAY_NO_COMMAND_ID = 3;
+	private static final int SAY_MAYBE_COMMAND_ID = 4;
+
+	private Command exitCommand = new Command("Exit", EXIT_COMMAND_ID);
+	private Command sayYesCommand = new Command("Say Yes", SAY_YES_COMMAND_ID);
+	private Command sayNoCommand = new Command("Say No", SAY_NO_COMMAND_ID);
+	private Command sayMaybeCommand = new Command("Say Maybe", SAY_MAYBE_COMMAND_ID);
+
+	public CommandByEqualsMIDlet() {
+		Display.init(this);
+	}
+	
+	protected void startApp() throws MIDletStateChangeException {
+		Form form = new Form("Command-By-Equals-MIDlet");
+		
+		form.addCommand(exitCommand);
+		form.addCommand(sayMaybeCommand);
+		form.addCommand(sayNoCommand);
+		form.addCommand(sayYesCommand);
+		
+		form.addCommandListener(this);
+		
+		form.show();
+	}
+
+	protected void destroyApp(boolean unconditional) throws MIDletStateChangeException {
+		notifyDestroyed();
+	}
+
+	protected void pauseApp() {
+	}
+
+	public void actionPerformed(ActionEvent event) {
+		Command command = event.getCommand();
+		int commandId = command.getId();
+		if(commandId == EXIT_COMMAND_ID) {
+			notifyDestroyed();
+		} else	
+		if(commandId == SAY_YES_COMMAND_ID) {
+			sayYes();
+		} else
+		if(commandId == SAY_NO_COMMAND_ID) {
+			sayNo();
+		} else
+		if(commandId == SAY_MAYBE_COMMAND_ID) {
+			sayMaybe();
+		}		
+	}
+
+	private void sayMaybe() {
+		Dialog.show("Maybe Command", "Maybe... :|", OK, CANCEL);
+	}
+
+	private void sayNo() {
+		Dialog.show("No Command", "No! :(", OK, CANCEL);
+	}
+
+	private void sayYes() {
+		Dialog.show("Yes Command", "Yes! :)", OK, CANCEL);
+	}
+}
